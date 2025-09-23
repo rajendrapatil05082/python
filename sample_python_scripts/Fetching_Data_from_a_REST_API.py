61c0c307a48a86be20f96db5cde58ad59d7cf7d1
Acecloudacademy <159706791+Acecloudacademy@users.noreply.github.com>
2025-09-23 07:01:31 +0530

    Update Fetching_Data_from_a_REST_API.py

--- sample_python_scripts/Fetching_Data_from_a_REST_API.py
+++ sample_python_scripts/Fetching_Data_from_a_REST_API.py
@@ -3,6 +3,9 @@ import requests
 response = requests.get('https://api.github.com/repos/python/cpython')
 data = response.json()
 
+
 print("Repository:", data['name'])
+
 print("Description:", data['description'])
+
 print("Stars:", data['stargazers_count'])
